c607603113e022efa053c88349a719435ff1cdbd
0verv0ice <theznoy@gmail.com>
2022-03-24 18:30:03 +0500

    task-cycles

--- src/Cycles.js
+++ src/Cycles.js
@@ -5,7 +5,7 @@
 */
 export function rangeSum(start, end) {
     let sum = 0;
-    for (let i = star; i <= end; i += 1) {
+    for (let i = start; i <= end; i += 1) {
         if (i % 2 === 0) {
             sum += i;
         }
